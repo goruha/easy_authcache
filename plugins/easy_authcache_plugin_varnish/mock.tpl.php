4a0f06de05660e2f358b24698090f43df1b4dd0a
Rodonov Igor <igorro@234004.no-reply.drupal.org>
2011-11-05 18:37:24 +0700

    Merge from github development branch dev 2

---
+++ plugins/easy_authcache_plugin_varnish/mock.tpl.php
@@ -0,0 +1,14 @@
+<?php
+/* $Id$ */
+
+/**
+ * @file
+ *
+ * Template used as mock up that will be replaced with ajax result
+ *  
+ */
+?>
+<esi:include src="<?php print $path; ?>"/>
+<!--<?php if (isset($debug)) :?>
+<?php print $debug; ?>
+<?php endif;?>-->
