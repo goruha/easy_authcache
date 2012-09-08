133c7b382d6aa3317592ff48e191d9a2ae6a5eb2
xt <xt@shinobi.(none)>
2012-09-09 02:13:52 +0700

    added temporary templates code

--- plugins/easy_authcache_plugin_varnish/easy_authcache_vcl_generator/templates/vcl-base-3.tpl.php
+++ plugins/easy_authcache_plugin_varnish/easy_authcache_vcl_generator/templates/vcl-base-3.tpl.php
@@ -6,3 +6,174 @@
  * Time: 1:48 AM
  * To change this template use File | Settings | File Templates.
  */
+
+backend default {
+	.host = "127.0.0.1";
+	.port = "80";
+	.connect_timeout = 600s;
+	.first_byte_timeout = 600s;
+	.between_bytes_timeout = 600s;
+}
+
+include "./eac.vcl";
+
+sub vcl_recv {
+	// Normalize the Accept-Encoding header
+	// as per: http://varnish-cache.org/wiki/FAQ/Compression
+	if (req.http.Accept-Encoding) {
+		if (req.url ~ "(?i)\.(flv|swf|mp3|mp4|m4a|ogg|mov|avi|wmv|jpe?g|png|gif|gz|tgz|bz2|tbz|mp3|ogg|eot|woff|ttf|htc)(\?.*)?$") {
+			# No point in compressing these
+			remove req.http.Accept-Encoding;
+		}
+		elsif (req.http.Accept-Encoding ~ "gzip") {
+			set req.http.Accept-Encoding = "gzip";
+		}
+		else {
+			# unkown algorithm
+			remove req.http.Accept-Encoding;
+		}
+	}
+
+	call eac_auth_recv;
+
+	remove req.http.X-Forwarded-For;
+	set req.http.X-Forwarded-For = client.ip;
+	remove req.http.X-Real-Forwarded-For;
+	set req.http.X-Real-Forwarded-For = client.ip;
+	remove req.http.X-Varnish-Client-IP;
+	set req.http.X-Varnish-Client-IP = client.ip;
+
+	set req.grace = 6h;
+
+	if (req.request == "PURGE"){
+		if (!client.ip ~ internal){
+			error 400 "Bad request.";
+		}
+		return(lookup);
+	}
+
+	if (!req.request ~ "GET|HEAD|PUT|POST|TRACE|OPTIONS|DELETE") {
+		error 400 "Bad request";
+	}
+
+	if (req.request != "GET" && req.request != "HEAD") {
+		return (pass);
+	}
+
+	if (req.url ~ "^/(cron|install|update)\.php") {
+		if (!client.ip ~ internal) {
+			error 401 "Restricted";
+		}
+		return(pass);
+	}
+
+	call eac_protected_recv;
+	call eac_static_cache_recv;
+	call eac_static_pass_recv;
+	call eac_urls_pass_recv;
+	call eac_cookie_pass_recv;
+
+
+	unset req.http.Cookie;
+	return (lookup);
+
+} # vcl_recv
+
+sub vcl_hash {
+	hash_data(req.url);
+
+	call eac_hash;
+
+	if (req.http.host) {
+		hash_data(req.http.host);
+	}
+	else {
+		hash_data(server.ip);
+	}
+
+	return (hash);
+} # vcl_hash
+
+sub vcl_fetch {
+	# compression, vcl_miss/vcl_pass unset compression from the backend
+	if (req.http.Accept-Encoding) {
+		set beresp.do_gzip = true;
+	}
+
+	#TTL
+	call eac_global_ttl_fetch;
+	set beresp.grace = 6h;
+
+	call eac_static_fetch;
+
+	call eac_esi_ttl_fetch;
+
+	call eac_headers_fetch;
+
+	unset beresp.http.Vary;
+
+	return(deliver);
+} # vcl_fetch
+
+sub vcl_deliver {
+	if (obj.hits > 0) {
+		set resp.http.X-Cache = "HIT";
+	}
+	else {
+		set resp.http.X-Cache = "MISS";
+	}
+	remove resp.http.X-Varnish-IP;
+	unset resp.http.X-Varnish;
+	unset resp.http.X-Powered-By;
+	unset resp.http.X-Drupal-Cache;
+	unset resp.http.X-TTL;
+	unset resp.http.X-Generator;
+	unset resp.http.server;
+	unset resp.http.Via;
+
+	return (deliver);
+} # vcl_deliver
+
+sub vcl_pass {
+	set bereq.http.X-Real-IP = client.ip;
+	set bereq.http.X-Forwarded-For = client.ip;
+	# compression done in vcl_fetch
+	unset bereq.http.accept-encoding;
+
+	return (pass);
+} # vcl_pass
+
+sub vcl_miss {
+	if (req.request == "PURGE") {
+		error 404 "Not in cache.";
+	}
+	# compression done in vcl_fetch
+	unset bereq.http.accept-encoding;
+
+	return (fetch);
+} # vcl_miss
+
+sub vcl_hit {
+	if (req.request == "PURGE") {
+		#set obj.ttl = 0s;
+		purge;
+		error 200 "Purged.";
+	}
+
+	return (deliver);
+} # vcl_hit
+
+sub vcl_error {
+	call eac_auth_error;
+	call eac_bad_request_error;
+	call eac_backend_server_error;
+} # vcl_error
+
+sub vcl_pipe {
+	# compression done in vcl_fetch
+	unset bereq.http.accept-encoding;
+	# http://www.varnish-cache.org/ticket/451
+	# This forces every pipe request to be the first one.
+	set bereq.http.connection = "close";
+} # vcl_pipe
+
--- plugins/easy_authcache_plugin_varnish/easy_authcache_vcl_generator/templates/vcl-eac-3.tpl.php
+++ plugins/easy_authcache_plugin_varnish/easy_authcache_vcl_generator/templates/vcl-eac-3.tpl.php
@@ -6,3 +6,213 @@
  * Time: 1:48 AM
  * To change this template use File | Settings | File Templates.
  */
+acl internal {
+	"127.0.0.1";
+	"localhost";
+}
+
+
+
+sub eac_auth_recv {
+	set req.http.X-Acl = "1";
+	if (req.http.X-Acl == "1") {
+		if (! client.ip ~ internal) {
+			if (! req.http.Authorization == "Basic ZnR2ZW46cmVnaW9ucw==") {
+				error 401 "Restricted";
+			}
+		}
+	}	
+}
+
+
+
+sub eac_auth_error {
+	if (obj.status == 401) {
+		set obj.http.Content-Type = "text/html; charset=utf-8";
+		set obj.http.WWW-Authenticate = "Basic realm=Secured";
+		synthetic {"
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+ "http://www.w3.org/TR/1999/REC-html401-19991224/loose.dtd">
+<HTML>
+  <HEAD>
+    <TITLE>Error</TITLE>
+    <META HTTP-EQUIV='Content-Type' CONTENT='text/html;'>
+  </HEAD>
+  <BODY><H1>401 Unauthorized (varnish).</H1></BODY>
+</HTML>
+		"};
+		return (deliver);
+	}
+}
+
+
+
+sub eac_bad_request_error {
+	if (obj.status == 400) {
+		set obj.http.Content-Type = "text/html; charset=utf-8";
+		synthetic {"
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+  <head>
+    <title>400 Bad request</title>
+  </head>
+  <body>
+    <h1>Error 400 Bad request</h1>
+    <p>Bad request</p>
+  </body>
+</html>
+		"};
+
+	return (deliver);
+	}
+}
+
+
+
+sub eac_backend_server_error {
+	if (obj.status >= 500 && obj.status <= 505) {
+		set obj.http.Content-Type = "text/html; charset=utf-8";
+		synthetic {"
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+  <head>
+    <title>Internal server error</title>
+  </head>
+  <body>
+    <h1>Internal server error</h1>
+  </body>
+</html>
+		"};
+
+	return (deliver);
+	}
+}
+
+
+
+sub eac_hash {
+	set req.http.EAC-ROLE-SESSION-ID = regsub( req.http.Cookie, "^.*?EACSESS(.{32})=([^;]*);*.*$", "\1\2" );
+	hash_data(req.http.EAC-ROLE-SESSION-ID);
+
+	set req.http.EAC-ESI-SESSION-ID = regsub( req.http.Cookie, "^.*?EACESISESS(.{32})=([^;]*);*.*$", "\1\2" );
+	if (req.url ~ "^/esi/") {
+		hash_data(req.http.EAC-ESI-SESSION-ID);
+	}
+
+}
+
+
+
+sub eac_protected_recv {
+	if (req.url ~ "(?i)\.(module|info|inc|profile|engine|test|po|txt|theme|svn|git|tpl(\.php)?)(\?.*|)$"
+	&& !req.url ~ "(?i)robots\.txt"
+	) {
+		if (!client.ip ~ internal) {
+			error 400 "Bad request";
+		}
+	}
+}
+
+
+
+sub eac_static_cache_recv {
+	if (req.url ~ "(?i)\.(jpeg|jpg|png|gif|ico|swf|js|css|txt|html|htm|pdf|tar|gz|gzip|bz2)(\?.*|)$") {
+		return (lookup);
+	}
+}
+
+
+
+sub eac_static_pass_recv {
+	if (req.url ~ "(?i)\.(jpeg|jpg|png|gif|ico|swf|js|css|txt|html|htm|pdf|tar|gz|gzip|bz2)(\?.*|)$") {
+		return (pass);
+	}
+}
+
+
+
+sub eac_urls_pass_recv {
+	if (req.url ~ "^/user"
+	|| req.url ~ "^/admin"
+	|| req.url ~ "^/logout"
+	) {
+		return(pass);
+	}
+}
+
+
+
+sub eac_cookie_pass_recv {
+	if (req.http.Cookie) {
+		set req.http.Cookie = ";" + req.http.Cookie;
+		set req.http.Cookie = regsuball(req.http.Cookie, "; +", ";");
+		set req.http.Cookie = regsuball(req.http.Cookie, ";(drupal_uid|SESS[a-z0-9]+)=", "; \1=");
+		set req.http.Cookie = regsuball(req.http.Cookie, ";[^ ][^;]*", "");
+		set req.http.Cookie = regsuball(req.http.Cookie, "^[; ]+|[; ]+$", "");
+
+		if (req.http.Cookie == "") {
+			unset req.http.Cookie;
+		}
+		else {
+			return (pass);
+		}
+	}
+}
+
+
+
+sub eac_global_ttl_fetch {
+	set beresp.ttl = 60m;
+}
+
+
+
+sub eac_static_fetch {
+	if (req.url ~ "(?i)\.(jpeg|jpg|png|gif|ico|swf|js|css|txt|html|htm|pdf)(\?.*|)$") {
+		unset beresp.http.set-cookie;
+		return(deliver);
+	}
+	else {
+		set beresp.do_esi = true;
+		remove beresp.http.expires;
+	}
+}
+
+
+
+sub eac_esi_ttl_fetch {
+	if (req.url ~ "^/esi") {
+		#unset beresp.http.set-cookie;
+		set beresp.do_gunzip = true;
+		set beresp.ttl = 10m;
+	}
+}
+
+
+
+sub eac_headers_fetch {
+	if (beresp.ttl <= 0s) {
+		set beresp.http.X-Cacheable = "NO:Not Cacheable";
+	}
+	elsif (req.http.Cookie ~ "(SESS[a-z0-9]+|drupal_uid)") {
+		set beresp.http.X-Cacheable = "NO:Got Session";
+		return(hit_for_pass);
+	}
+	elsif ( beresp.http.Cache-Control ~ "private") {
+		set beresp.http.X-Cacheable = "NO:Cache-Control=private";
+		return(hit_for_pass);
+	}
+	elsif ( beresp.ttl < 1s ) {
+		set beresp.ttl   = 5s;
+		set beresp.grace = 5s;
+		set beresp.http.X-Cacheable = "YES:FORCED";
+	}
+	else {
+		set beresp.http.X-Cacheable = "YES";
+	}
+}
+
